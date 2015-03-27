f260cea89a1858af91fcb4c9a558da7d032bcef4
WangWenjun559 <wenjunw@cs.cmu.edu>
2015-03-27 16:05:48 -0400

    Create merge.py

---
+++ coref/merge.py
@@ -0,0 +1,69 @@
+import sys
+import glob
+import errno
+import codecs
+
+def sortFile(fileList):
+	quickSortHelper(fileList,0,len(fileList)-1)
+
+def quickSortHelper(fileList,start,end):
+	if start < end:
+		pivot = partition(fileList,start,end)
+
+		quickSortHelper(fileList,start,pivot-1)
+		quickSortHelper(fileList,pivot+1,end)
+
+def partition(fileList,start,end):
+	pivot = int((fileList[start].split('/')[-1]).split('.')[0])
+
+	left = start + 1
+	right = end
+
+	done = False
+
+	while not done:
+		while left <= right and \
+			int((fileList[left].split('/')[-1]).split('.')[0]) <= pivot:
+		 	left += 1
+
+		while int((fileList[right].split('/')[-1]).split('.')[0]) >= pivot \
+			and right >= left:
+		 	right -= 1
+
+		if right < left:
+		 	done = True
+		else:
+			temp = fileList[left]
+			fileList[left] = fileList[right]
+			fileList[right] = temp
+
+	temp = fileList[start]
+	fileList[start] = fileList[right]
+	fileList[right] = temp
+
+	return right
+
+def main():
+	filepath = sys.argv[1] # THIS MAY CHANGE
+	filepath += "/*.txt" 
+	files = glob.glob(filepath)
+	sortFile(files)
+	one = "processed/" + (filepath.split('/')[1]).split('_')[1] + ".txt"
+	print "merge " + one
+	fmerge = open(one,'a') # THIS MAY CHANGE
+
+	for name in files:
+		#print name
+		try:
+			with open(name) as f:
+				body = f.read()
+				body = body + '\n\n'
+				fmerge.write(body)
+		except IOError as exc:
+			if exc.errno != errno.EISDIR:
+				raise
+
+	fmerge.close()
+
+if __name__ == '__main__':
+	main()
