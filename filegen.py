ca3600c94488bb1d806b1254071f2bb1709b031b
WangWenjun559 <wenjunw@cs.cmu.edu>
2015-03-27 15:28:33 -0400

    split files

--- filegen.py
+++ filegen.py
@@ -1,8 +1,13 @@
+import sys
+
+filename = sys.argv[1]
+
 i = 0
-for line in open('data/a1.txt'):
+for line in open(filename):
 	if len(line.strip())==0:continue
 	if len(line.split())<2:continue
-	fw = open('data/splits/a1_'+str(i)+'.txt','w')
+	split = 'data/split_' + filename.split("/")[-1].split(".")[0] + '/' + str(i) + '.txt'
+	fw = open(split,'w')
 	fw.write(line)
 	fw.close()
 	i+=1
