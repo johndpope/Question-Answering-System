9063bd5d7a05f29bce5e382d7a8360a9b31004e9
WangWenjun559 <wenjunw@cs.cmu.edu>
2015-03-27 15:29:42 -0400

    main part of coref

--- preprocess.py
+++ preprocess.py
@@ -38,7 +38,8 @@ def resolution(pron,id_term,root):
 def main():
 	pron = set(['she','her','hers','he','him','his','it','its','they','them','their',\
 					'i','me','my','mine','we','our','ours','us','you','your','yours'])
-	datapath = "xml/a1/*.tagged" # may change
+	datapath = sys.argv[1] # may change
+	datapath += "/*.tagged" 
 	xmlfile = glob.glob(datapath)
 	for name in xmlfile:
 		print name
@@ -56,11 +57,15 @@ def main():
 				to_write = resolution(pron,id_term,root)
 				
 		except Exception as exc:
-			#print "exception in file: %s" % name 
+			print "exception in file: %s" % name 
+			name = name.replace("tagged","txt")
 			to_write = codecs.open(name,encoding='utf-8').read()
 		#print to_write
 		
-		fprocess = codecs.open("processed/a1/"+name.split("/")[-1].split('.')[0]+".txt",'w','utf-8') #may change
+		towritename = name.replace("split","processed")
+		towritename = towritename.replace("tagged","txt")
+		print towritename
+		fprocess = codecs.open(towritename,'w','utf-8') #may change
 		fprocess.write(to_write)
 		fprocess.close()
 
